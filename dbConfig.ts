3a49bc8ac683fb6909c1c3ff3e2e4e19d442ed9d
Muhammad Ilham Purnama <ilhampurnama78@gmail.com>
2025-11-18 16:04:46 +0700

    feat: integrate TypeORM with PostgreSQL and add Property entity

---
+++ dbConfig.ts
@@ -0,0 +1,14 @@
+import { Property } from "src/entities/property.entity";
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions.js";
+
+export const pgConfig:PostgresConnectionOptions = {
+  type: "postgres",
+  database:'project_nestjs',
+  username: 'postgres',
+  password: 'postgres',
+  host:'localhost',
+  port: 5432,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: true, // to synchronize our schema with actual field in db. ok in dev but carefull in production. can cause permanent data loss, i
+  // logging: true
+}
